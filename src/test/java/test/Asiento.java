bc81ffb57ae85140d0e5911b22ef2048c7fe5049
Mgarciaqu <124626393+Mgarciaqu@users.noreply.github.com>
2023-02-21 11:52:22 -0500

    firstjava

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,9 @@
+public class Asiento{
+     String color;
+     int precio;
+     int registro;
+    void cambiarColor(String color){
+          if (color == "amarillo" ||color == "rojo" ||color == "verde" ||color == "negro" ||color == "blanco"){
+               this.color = color
+     }
+}
